70ae8b851aa6a238ab2f31ba3af76962d3419134
Juliana Cespedes <cespedesjuliana115@gmail.com>
2026-05-31 18:44:13 -0500

    update

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8011',
+        target: 'http://localhost:8001',
         changeOrigin: true,
       },
     },
